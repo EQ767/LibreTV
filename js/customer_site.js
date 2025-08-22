3919b4c6b6ff1ea9732080c4f1f3bda0592f69af
EQ767 <zyqcmbs@gmail.com>
2025-08-23 00:09:04 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,7 @@
 const CUSTOMER_SITES = {
     qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+        api: 'https://cj.lziapi.com/api.php/provide/vod/from/lzm3u8/at/json/',
+        name: '量子资源',
     }
 };
 
